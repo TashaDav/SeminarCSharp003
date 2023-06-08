ff87bc4dbc8c75e4ff46c9d8472bbc6475855a8e
davydova <natalya-davydova-86@mail.ru>
2023-06-08 18:11:38 +0300

    Chenged Task 022

--- Task022/Program.cs
+++ Task022/Program.cs
@@ -3,10 +3,22 @@
 и выдает таблицу квадратов чисел от 1 до N. 
 */
 
+/*
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= number; i++)
 {
     Console.Write($"{i * i} ");
+}
+*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+for(int i = 0; i <= N; i++)
+{
+    array[i] = (i + 1) * (i + 1);
+    Console.WriteLine(array[i]);
 }
