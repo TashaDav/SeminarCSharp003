f9e59aba121a247146fc3ed747451d124840c60d
davydova <natalya-davydova-86@mail.ru>
2023-06-08 16:45:42 +0300

    Created Task018

---
+++ Task018/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Написать программу, которая  по заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y)
+*/
+
+
+Console.WriteLine("Введите номер четверти: ");
+int quator = Convert.ToInt32(Console.ReadLine());
+if (quator == 1)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (quator == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (quator == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (quator == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("Задана неверная четверть.");
+}
