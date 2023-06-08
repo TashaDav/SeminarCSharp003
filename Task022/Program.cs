e702ea86c40b9689e306718665d2660498f8800d
davydova <natalya-davydova-86@mail.ru>
2023-06-08 17:44:35 +0300

    Created Task022

---
+++ Task022/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Написать программу, которая принимает на вход число (N)
+и выдает таблицу квадратов чисел от 1 до N. 
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i} ");
+}
